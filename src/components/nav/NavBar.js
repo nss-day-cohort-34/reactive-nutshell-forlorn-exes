b3cfffc094e6d73a3d5b80daae6377978dbba871
KevinSadler <KevinSadler@github.comgit config --global user.email KevinSadler@github.comgit config --global user.email KevinSadler@github.com>
2019-08-30 15:52:12 -0500

    bug fix

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,8 +8,12 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
                 <ul className="nav nav-pills nav-fill">
+
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">News</Link>
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/news">News</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/friends">Friends</Link>
@@ -19,6 +23,9 @@ class NavBar extends Component {
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/tasks">Tasks</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/events">Events</Link>
                     </li>
                 </ul>
             </nav>
